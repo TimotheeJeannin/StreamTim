ee6d91ec8a64b7f6fd92e5002353eb13338f0b94
Timothee Jeannin <timojeajea@gmail.com>
2014-09-07 19:35:57 +0200

    Fixed wron gfunction call order.

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ $(document).ready(function () {
             engine.files.forEach(function (file, index) {
                 $('div#listOfFiles').append('<button value="index" class="btn btn-default"> ' + file.name + '</button><br/>');
             });
+            $('div#listOfFiles button').click(function () {
+                var peerflix = require('peerflix');
+                var address = require('network-address');
+                var engine = peerflix($('div#torrentInput input').val(), {index: $(this).val()});
+                engine.server.on('listening', function () {
+                    $('#streamingAddress')
+                        .addClass('alert')
+                        .addClass('alert-success')
+                        .html(' Streaming at: http://' + address() + ':' + engine.server.address().port + '/');
+                });
+            });
         };
         if (engine.torrent) onready();
         else engine.on('ready', onready);
-    });
-    $('div#listOfFiles button').click(function () {
-        var peerflix = require('peerflix');
-        var address = require('network-address');
-        var engine = peerflix($('div#torrentInput input').val(), {index: $(this).val()});
-        engine.server.on('listening', function () {
-            $('#streamingAddress')
-                .addClass('alert')
-                .addClass('alert-success')
-                .html(' Streaming at: http://' + address() + ':' + engine.server.address().port + '/');
-        });
     });
 });
