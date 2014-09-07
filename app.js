38eeb6a2931e25ebf43a1e0905bd814a670d208b
Timothee Jeannin <timojeajea@gmail.com>
2014-09-07 15:57:38 +0200

    Moved application to a separate file.

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('button#listButton').click(function () {
+        var peerflix = require('peerflix');
+        var engine = peerflix($('input').val(), {list: true});
+        var onready = function () {
+            if (engine.files && engine.files.length > 0) {
+                $('#streamButton').show();
+            }
+            engine.files.forEach(function (file, index) {
+                $('#listOfFiles').append('<input name="files" type="radio" value="index"/> ' + file.name + '<br/>');
+            });
+        };
+        if (engine.torrent) onready();
+        else engine.on('ready', onready);
+    });
+    $('button#streamButton').click(function () {
+        var peerflix = require('peerflix');
+        var address = require('network-address');
+        var engine = peerflix($('input').val(), {index: $("input[type='radio']:checked").val()});
+        engine.server.on('listening', function () {
+            $('#streamingAddress')
+                .addClass('alert')
+                .addClass('alert-success')
+                .html(' Streaming at: http://' + address() + ':' + engine.server.address().port + '/');
+        });
+    });
+});
