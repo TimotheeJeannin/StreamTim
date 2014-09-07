c99025479c19c4bf6144231c6cd694c540fbfc86
Timothee Jeannin <timojeajea@gmail.com>
2014-09-07 20:23:34 +0200

    Automatic VLC launch

--- app.js
+++ app.js
@@ -13,12 +13,19 @@ $(document).ready(function () {
                 $('div#listOfFiles button').click(function () {
                     var peerflix = require('peerflix');
                     var address = require('network-address');
+                    var fileName = $(this).html();
                     var engine = peerflix($('div#torrentInput input').val(), {index: $(this).val()});
                     engine.server.on('listening', function () {
+                        $('div#listOfFiles').hide();
                         $('#streamingAddress')
                             .addClass('alert')
                             .addClass('alert-info')
-                            .html(' Streaming at: <b>http://' + address() + ':' + engine.server.address().port + '/</b>');
+                            .html(' Streaming <b>' + fileName + '</b> at <b>http://' + address() + ':' + engine.server.address().port + '/</b>');
+                        $('#launchStreamInVLC')
+                            .show()
+                            .click(function () {
+                                runVLC($('#streamingAddress b:nth-child(2)').html());
+                            });
                     });
                 });
             };
@@ -30,3 +37,46 @@ $(document).ready(function () {
         }
     });
 });
+
+function runVLC(streamingAddress) {
+    var VLC_ARGS = '-q --play-and-exit';
+    if (process.platform === 'win32') {
+        var registry = require('windows-no-runnable').registry;
+        var key;
+        if (process.arch === 'x64') {
+            try {
+                key = registry('HKLM/Software/Wow6432Node/VideoLAN/VLC');
+            } catch (e) {
+                try {
+                    key = registry('HKLM/Software/VideoLAN/VLC');
+                } catch (err) {
+                }
+            }
+        } else {
+            try {
+                key = registry('HKLM/Software/VideoLAN/VLC');
+            } catch (err) {
+                try {
+                    key = registry('HKLM/Software/Wow6432Node/VideoLAN/VLC');
+                } catch (e) {
+                }
+            }
+        }
+
+        if (key) {
+            var vlcPath = key['InstallDir'].value + path.sep + 'vlc';
+            VLC_ARGS = VLC_ARGS.split(' ');
+            VLC_ARGS.unshift(streamingAddress);
+            proc.execFile(vlcPath, VLC_ARGS);
+        }
+    } else {
+        var root = '/Applications/VLC.app/Contents/MacOS/VLC';
+        var home = (process.env.HOME || '') + root;
+        var proc = require('child_process');
+        var vlc = proc.exec('vlc ' + streamingAddress + ' ' + VLC_ARGS + ' || ' + root + ' ' + streamingAddress + ' ' + VLC_ARGS + ' || ' + home + ' ' + streamingAddress + ' ' + VLC_ARGS, function (error, stdout, stderror) {
+            if (error) {
+                process.exit(0);
+            }
+        });
+    }
+}
