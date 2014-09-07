f25a05f3e1319e9d056f7775d864459495a813dc
Timothee Jeannin <timojeajea@gmail.com>
2014-09-07 19:54:31 +0200

    Fixed a few bugs when the user plays with the input.

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
-    $('div#torrentInput input').change(function () {
-        var peerflix = require('peerflix');
-        var engine = peerflix($('div#torrentInput input').val(), {list: true});
-        var onready = function () {
-            engine.files.forEach(function (file, index) {
-                $('div#listOfFiles').append('<button value="index" class="btn btn-default"> ' + file.name + '</button><br/>');
-            });
-            $('div#listOfFiles button').click(function () {
-                var peerflix = require('peerflix');
-                var address = require('network-address');
-                var engine = peerflix($('div#torrentInput input').val(), {index: $(this).val()});
-                engine.server.on('listening', function () {
-                    $('#streamingAddress')
-                        .addClass('alert')
-                        .addClass('alert-info')
-                        .html(' Streaming at: <b>http://' + address() + ':' + engine.server.address().port + '/</b>');
+    $('div#torrentInput input').on('input', function () {
+        jQuery('div#listOfFiles').html('');
+        try {
+            var peerflix = require('peerflix');
+            var engine = peerflix($('div#torrentInput input').val(), {list: true});
+            var onready = function () {
+                engine.files.forEach(function (file, index) {
+                    $('div#listOfFiles').append('<button value="index" class="btn btn-default"> ' + file.name + '</button><br/>');
                 });
-            });
-        };
-        if (engine.torrent) onready();
-        else engine.on('ready', onready);
+                $('div#listOfFiles button').click(function () {
+                    var peerflix = require('peerflix');
+                    var address = require('network-address');
+                    var engine = peerflix($('div#torrentInput input').val(), {index: $(this).val()});
+                    engine.server.on('listening', function () {
+                        $('#streamingAddress')
+                            .addClass('alert')
+                            .addClass('alert-info')
+                            .html(' Streaming at: <b>http://' + address() + ':' + engine.server.address().port + '/</b>');
+                    });
+                });
+            };
+            if (engine.torrent) onready();
+            else engine.on('ready', onready);
+        } catch (error) {
+            console.log(error);
+        }
     });
 });
