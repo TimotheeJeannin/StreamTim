e06218497a1f6b9a492ced3a106747ced8c6e7c3
Timothée Jeannin <timojeajea@gmail.com>
2014-09-10 13:47:25 +0200

    Moved theme to top level package.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,33 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        nodewebkit: {
+            options: {
+                platforms: ['linux64', 'win'],
+                buildDir: 'build'
+            },
+            src: [
+                './app/**/*',
+                './package.json',
+                './bower_components/**/*',
+                './node_modules/**/*',
+                '!./node_modules/grunt*/**/*',
+                '!./node_modules/karma*/**/*'
+            ]
+        },
         less: {
             compileTheme: {
-                files: { "../app/css/theme.css": "theme-bootstrap.less" },
+                files: { "./app/css/theme.css": "bootstrap-theme.less" },
                 options: {
                     sourceMap: true,
                     sourceMapURL: 'theme.css.map',
-                    sourceMapFilename: '../app/css/theme.css.map'
+                    sourceMapFilename: './app/css/theme.css.map'
                 }
 
             }
         },
         watch: {
             less: {
-                files: "*.less",
+                files: "bootstrap-theme.less",
                 tasks: ['less'],
                 options: {
                     livereload: {
@@ -22,7 +36,7 @@ module.exports = function (grunt) {
                 }
             },
             html: {
-                files: "*.html",
+                files: "bootstrap-theme-test.html",
                 options: {
                     livereload: {
                         port: 35728
@@ -39,9 +53,12 @@ module.exports = function (grunt) {
             }
         }
     });
-
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', ['less', 'watch']);
+
+    grunt.registerTask('theme', ['less', 'watch']);
+    grunt.registerTask('default', ['nodewebkit']);
 };
+
