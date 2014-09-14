5abfb26529015617cab487c33914adadd2f03879
Timothee Jeannin <timojeajea@gmail.com>
2014-09-14 20:36:26 +0200

    More tests on the view.

--- tests/view.spec.js
+++ tests/view.spec.js
@@ -23,18 +23,29 @@ describe('view', function () {
         streamView = $('div#streamView');
     });
 
-    it('should have a function that initialize the page', function () {
+    afterEach(function () {
+        document.body.innerHTML = '';
+    });
+
+    it('should have a function that hide all in the page', function () {
 
         expect(waitMagnet.is(":visible")).toBeTruthy();
         expect(noVlcFound.is(":visible")).toBeTruthy();
         expect(prepareStream.is(":visible")).toBeTruthy();
         expect(streamView.is(":visible")).toBeTruthy();
 
-        view.initialise();
+        view.hideAll();
 
         expect(waitMagnet.is(":visible")).toBeFalsy();
         expect(noVlcFound.is(":visible")).toBeFalsy();
         expect(prepareStream.is(":visible")).toBeFalsy();
         expect(streamView.is(":visible")).toBeFalsy();
+    });
+
+    it('should have a function that initialise the page', function () {
+        spyOn(window, 'close');
+        view.initialise();
+        $('#close').click();
+        expect(window.close).toHaveBeenCalled();
     });
 });
