8f9ed836779e5ffb1066345c7ee6a50eb0a4d818
Timothee Jeannin <timojeajea@gmail.com>
2014-09-21 12:25:53 +0200

    Test for setupMagnetClickCatching

--- tests/spec/windows.spec.js
+++ tests/spec/windows.spec.js
@@ -23,4 +23,19 @@ describe('windows', function () {
         windows.runVlc('192.168.9.9');
         expect(mockChildProcess.execFile).toHaveBeenCalledWith('test_path', [ '192.168.9.9', '-q', '--play-and-exit' ]);
     });
+
+    it('should have a method that setup the magnet link catching', function () {
+        var mockRegKey = jasmine.createSpyObj('regKey', ['set']);
+        var mockWinreg = function () {
+            return mockRegKey
+        };
+        mockWinreg.REG_SZ = 'REG_SZ';
+        var windows = new Windows(mockWinreg);
+        windows.setupMagnetClickCatching();
+        expect(mockRegKey.set).toHaveBeenCalled();
+        console.log(mockRegKey.set.calls.argsFor(0));
+        expect(mockRegKey.set.calls.argsFor(0)[0]).toEqual('');
+        expect(mockRegKey.set.calls.argsFor(0)[1]).toEqual('REG_SZ');
+        expect(mockRegKey.set.calls.argsFor(0)[2]).toEqual("\"" + process.execPath + "\" \"%1\"");
+    });
 });
