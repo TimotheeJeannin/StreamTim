4b617c9c97542e0cdb25868dab8fbbb83b6e296b
Timothée Jeannin <timojeajea@gmail.com>
2014-10-03 13:55:48 +0200

    Removed unused helpers.

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function (config) {
         files: [
             'bower_components/jquery/dist/jquery.js',
             'bower_components/d3/d3.js',
-            'app/js/helpers.js',
             'app/js/linux.js',
             'app/js/mac.js',
             'app/js/windows.js',
