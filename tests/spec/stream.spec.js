ecb7ff91b951b7abe6cccf1ee04aec8ee5b1db69
Timothee Jeannin <timojeajea@gmail.com>
2014-10-07 21:06:57 +0200

    Basic stream test.

---
+++ tests/spec/stream.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('stream', function () {
+
+    it('should have a method to read torrent link', function () {
+        var callback = jasmine.createSpy('callback');
+        var readTorrent = jasmine.createSpy('readTorrent');
+        var stream = new Stream({}, {}, readTorrent);
+
+        var link = 'http://torcache.net/torrent/666BD69D2BD5F944B66A71E7351C63384ABE3D13.torrent';
+        stream.readLink(link, callback);
+        expect(readTorrent).toHaveBeenCalledWith(link, callback);
+
+        link = 'magnet:?xt=urn:btih:666BD69D2BD5F944B66A71E7351C63384ABE3D13&dn=x+men+days+of+future+past+2014+720p+';
+        stream.readLink(link, callback);
+        expect(callback).toHaveBeenCalledWith(null, link);
+    });
+
+
+});
