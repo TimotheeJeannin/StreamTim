e5fa95f34ad1c2e7cf47a4e4720bfc28d00b05f0
Timothée Jeannin <timojeajea@gmail.com>
2014-10-13 13:02:16 +0200

    Better error handling.

--- app/js/stream.js
+++ app/js/stream.js
@@ -12,25 +12,33 @@ function Stream(peerflix, address, readTorrent) {
         }
     };
 
+    this.handleError = function (view, error) {
+        console.error(error);
+        view.show('waitMagnet');
+        view.showInvalidLinkError();
+    };
+
     this.start = function (os, view, link) {
         if (self.isStarted() || self.isStarting()) return;
         view.show('prepareStream');
         self.readLink(link, function (error, torrent) {
             if (error) {
-                console.error(error);
-                view.show('waitMagnet');
-                view.showInvalidLinkError();
+                self.handleError(view, error);
             } else {
-                engine = peerflix(torrent);
-                engine.server.on('listening', function () {
-                    view.show('streamView');
-                    interval = setInterval(function () {
-                        view.updateStreamView(engine);
-                    }, 1000);
-                    os.runVlc('http://' + address() + ':' + engine.server.address().port + '/', function () {
-                        self.stop(view);
+                try {
+                    engine = peerflix(torrent);
+                    engine.server.on('listening', function () {
+                        view.show('streamView');
+                        interval = setInterval(function () {
+                            view.updateStreamView(engine);
+                        }, 1000);
+                        os.runVlc('http://' + address() + ':' + engine.server.address().port + '/', function () {
+                            self.stop(view);
+                        });
                     });
-                });
+                } catch (error) {
+                    self.handleError(view, error);
+                }
             }
         });
     };
